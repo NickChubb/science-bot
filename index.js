072aa1987445f9faff1d779071df52a504ad32ef
Nick Chubb <Nick.r.chubb@gmail.com>
2020-09-12 10:39:38 -0700

    add config.json and update .gitignore

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 // index.js
 
 const Discord = require('discord.js');
+const { token, prefix, eventsChannelID, announcementsChannelID } = require('./config.json');
 const client = new Discord.Client();
 
+eventsChannel = client.channels.cache.get(eventsChannelID);
+announcementsChannel = client.channels.cache.get(announcementsChannelID);
+
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
 client.on('message', message => {
-	if (message.content === '~ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
+	if (message.content === `${prefix}add`) {
+
+        // POST request to database
+
+        // Update events channel
+
+        //
+
     }
 });
 
-client.login('NzU0MzcyNjMwNDM3NTYwNDMx.X1zyQg._l2OYdWYxZY6PDU4LPS_a0JJ1L4');
+function sendMessage(){
+    client.channels.cache.get(eventsChannelID).send("This text is being sent every minute");
+}
+
+/**
+ * Creates the message to be sent in the events channel
+ */
+function createCalendar(){
+    
+}
+
+setInterval(sendMessage, 60000);
+
+client.login(token);
