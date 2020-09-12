ebb42bb60db4e54395f5e482b98b519ba289ea74
Nick Chubb <Nick.r.chubb@gmail.com>
2020-09-12 09:54:42 -0700

    ping/pong

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // index.js
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '~ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+});
+
+client.login('NzU0MzcyNjMwNDM3NTYwNDMx.X1zyQg._l2OYdWYxZY6PDU4LPS_a0JJ1L4');
