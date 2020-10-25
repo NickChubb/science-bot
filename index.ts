9d469431e4cb986cc63a6c7d3f145602c800c574
Nick Chubb <Nick.r.chubb@gmail.com>
2020-10-25 13:50:26 -0700

    Fix createEventEmbed again

--- index.ts
+++ index.ts
@@ -335,7 +335,7 @@ function createEventEmbed(event){
         thumbnailUrl += 'psa.png';
     } else if (event.title.includes("Chemistry Student Society") || event.title.includes("CSS")) {
         thumbnailUrl += 'css.png';
-    } else if (event.title.includes("Simon Fraser Student Society") || event.title.includes("SFSS") || event.description.includes("SFSS")) {
+    } else if (event.title.beginsWith("Simon Fraser Student Society") || event.title.beginsWith("SFSS") || event.description.includes("SFSS")) {
         thumbnailUrl += 'sfss.png';
     } else {
         thumbnailUrl += 'sus.png';
