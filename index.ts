3db7f1fb54099b3a848aa87ef3380d0c6ff2cbc0
Nick Chubb <Nick.r.chubb@gmail.com>
2020-11-08 19:43:19 -0800

    fixing bugs

--- index.ts
+++ index.ts
@@ -91,7 +91,7 @@ client.on('message', async message => {
 
     if (!message.content.startsWith(`${prefix}`)) { return };
 
-    console.log(message.member.roles.cache.map(r => r.name));
+    console.log(message.member.roles.cache.map(r => modRoles.indexOf(r)));
     if (message.member.roles.cache.some(item => modRoles.indexOf(item) !== -1)) {
         message.reply('```diff\n- Sorry, only users with the following roles can use me: ' +  `${modRoles}` + '\n```');
         return;
